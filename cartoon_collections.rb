5f84bf6b87acd2cf59aa0724d36ecc2f6423406b
Pamela Frieze <pfrieze@gmail.com>
2017-10-11 22:53:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
-def roll_call_dwarves(name)
+def roll_call_dwarves(dwarves)
   # code an argument here
   # Your code here
   
-  puts "#{name}."
+  puts "#{dwarves}."
 end
 
 def summon_captain_planet# code an argument here
