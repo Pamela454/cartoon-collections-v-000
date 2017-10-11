d44c476d5dba6c364d5febee79b6188a2d669508
Pamela Frieze <pfrieze@gmail.com>
2017-10-11 23:15:19 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)
   # code an argument here
   # Your code here
-  dwarves.each do |dwarves, index|
+  dwarves.each_with_index |dwarves, index|
   puts "#{index}#{dwarves}."
   end
 end
