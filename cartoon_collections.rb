203b7d3a275359e3a71585f699afc43bb8211dd1
Pamela Frieze <pfrieze@gmail.com>
2017-10-12 02:00:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,8 @@ end
 def long_planeteer_calls(calls)# code an argument here
   # Your code here
   calls.each do |call|
-    call.length < 4
-  end 
+    "#{call.length < 4}"
+  end
 end
 
 def find_the_cheese# code an argument here
